51571bef86fdb52ea90baabf345f54117ef6d21b
qkrwnsqja <wnsgks0205@gmail.com>
2025-11-04 13:38:41 +0900

    Task

--- src/chp10/ex3/ClubCard.jsx
+++ src/chp10/ex3/ClubCard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import './ClubCard.css';
 
-// [수정된 부분]
-// 다운받을 이미지 파일 경로를 import 합니다.
-// (imgs 폴더에 club1.jpg, club2.jpg 등이 있다고 가정)
 import clubImg1 from './imgs/club1.jpg';
 import clubImg2 from './imgs/club2.jpg';
 import clubImg3 from './imgs/club3.jpg';
 import clubImg4 from './imgs/club4.jpg';
 
 const clubs = [
-    // [수정된 부분]
-    // avatar 값에 import한 변수(clubImg1)를 넣어줍니다.
+
     {id: 1, name:"코딩 동아리", description:"React와 Node.js 스터디", avatar: clubImg1},
     {id: 2, name:"사진 동아리", description:"매주 출사를 나갑니다", avatar: clubImg2},
     {id: 3, name:"농구 동아리", description:"점심시간에 농구할 사람", avatar: clubImg3},
